a62b84a9ccaea2e313e0eafaa7a16e825092ae8b
Fazlul Hoque <sawravm299@gmail.com>
2021-08-06 22:55:13 +0600

    player script update
    player move left and right

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,9 @@ public class PlayerScript : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     public float forwardSpeed = 10.0f;
+
+    private int desiredLane = 1; //0: left, 1:middle, 2:right
+    public float laneDistance = 4; // the distance between two lane
     void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -16,6 +19,34 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         direction.z = forwardSpeed;
+
+        //gather the input where lane we should be
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            desiredLane++;
+            if(desiredLane == 3)
+                desiredLane = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            desiredLane--;
+            if(desiredLane == -1)
+                desiredLane = 0;
+        }
+        //calculate where we should be in the future
+         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+         if(desiredLane == 0)
+         {
+             targetPosition += Vector3.left * laneDistance;
+         }
+         else if(desiredLane == 2)
+         {
+             targetPosition += Vector3.right * laneDistance;
+         }
+
+         transform.position = Vector3.Lerp(transform.position, targetPosition, 80* Time.deltaTime);
+
     }
 
     private void FixedUpdate()
